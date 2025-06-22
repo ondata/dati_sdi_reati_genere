32ebda0d74c009165c098f401043e3abe5cee2e9
aborruso <aborruso@gmail.com>
2025-06-22 17:36:07 +0200

    update

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -277,44 +277,69 @@ done
 find "${folder}"/../data/processing/"${file}" -type f -name "*.csv" | while read -r csv_file; do
 
   # Utilizza DuckDB per normalizzare i nomi delle colonne (es. spazi -> underscore, minuscole)
+# Questo comando legge il CSV originale e produce un nuovo file con nomi colonna normalizzati
   duckdb --csv -c "FROM read_csv('${csv_file}',normalize_names=true)" >"${folder}"/tmp/tmp.csv
 
+  # Sostituisce il file originale con la versione normalizzata
   mv "${folder}"/tmp/tmp.csv "${csv_file}"
 
-  # Rinomina le colonne s _value a value)
+  # Rinomina le colonne da _value a valore per uniformare la nomenclatura
   sed -i 's/,_value/,valore/g' "${csv_file}" # Escapes quotes in CSV files
 
-  # applica il sorting
+  # Applica il sorting dei dati per facilitare l'analisi e le query
   mlr -I --csv sort -t anno,provincia,delitto,descrizione_reato,eta_alla_data_del_reato_vittima "${csv_file}"
 
+  # Riordina le colonne per avere una struttura dati coerente tra i vari file
   mlr -I --csv reorder -f anno,provincia,delitto,descrizione_reato,eta_alla_data_del_reato_vittima "${csv_file}"
 
+  # Estrae i nomi delle province per creare un elenco completo
+  # Questo verrà utilizzato per la normalizzazione delle province
   mlr --c2n cut -f provincia then uniq -a "${csv_file}" >>"${folder}"/tmp/province.txt
 
 done
 
-## codici provincia
+# ===== FASE 4: NORMALIZZAZIONE PROVINCE =====
+# Estrazione e normalizzazione dei codici provincia dai dati SDI
 
+# Elabora e normalizza i nomi delle province trovate nei dati SDI
 mlr --inidx --ocsv --ifs tab uniq -a then skip-trivial-records then label provinciauts "${folder}"/tmp/province.txt | grep -viP "non localizzata" >"${folder}"/tmp.txt
 
+# Salva il file temporaneo come elenco delle province SDI
 mv "${folder}"/tmp.txt "${folder}"/tmp/province_sdi.csv
 
+# Aggiunge una colonna per i nomi corretti delle province (inizialmente uguale all'originale)
 mlr -I --csv put '$provinciauts_corretto=$provinciauts' "${folder}"/tmp/province_sdi.csv
 
+# Applica le correzioni ai nomi delle province utilizzando il file di mappatura JSONL
+# che contiene le correzioni per i problemi noti nei nomi delle province
 while IFS= read -r line; do
+  # Estrae il nome originale e quello corretto dal file JSONL
   nome=$(jq -r '.nome' <<<"${line}")
   nome_corretto=$(jq -r '.nome_corretto' <<<"${line}")
 
+  # Sostituisce il nome errato con quello corretto nel campo 'provinciauts_corretto'
   mlr -I --csv sub -f provinciauts_corretto "^${nome}$" "${nome_corretto}" "${folder}"/tmp/province_sdi.csv
 done <"${folder}"/../resources/problemi_nomi_province.jsonl
 
+# Estrae le colonne rilevanti dalle unità territoriali ISTAT e le salva in un file temporaneo
+# per essere utilizzate nel join con le province SDI
 mlr --csv cut -f codice_provinciauts,provinciauts,flag_tipo_uts,codice_provincia_storico,codice_regione,ripartizione_geografica,regione,sigla_automobilistica then uniq -a "${folder}"/../resources/unita_territoriali_istat.csv >>"${folder}"/tmp/province_istat.csv
 
+# Effettua il join fuzzy tra le province SDI e quelle ISTAT utilizzando l'algoritmo Levenshtein
+# con soglia di similarità 0.9 e join left-outer per mantenere tutte le province SDI
 csvmatch "${folder}"/tmp/province_sdi.csv "${folder}"/tmp/province_istat.csv --fields1 provinciauts_corretto --fields2 provinciauts --fuzzy levenshtein -r 0.9 -i -a -n --join left-outer --output '1*' '2*' >"${folder}"/../resources/province_sdi_istat.csv
 
-mlr -I --csv rename provinciauts,provincia "${folder}"/../resources/province_sdi_istat.csv
+# Rinomina il campo 'provinciauts' in 'provincia' per uniformare la nomenclatura
+mlr -I --csv rename provinciauts,provincia then uniq -a "${folder}"/../resources/province_sdi_istat.csv
 
+# Arricchimento dei dati con le informazioni geografiche
+# Per ogni file CSV (tranne 'omicidi_dcpc.csv'), esegue un join con il file delle province
+# per aggiungere le informazioni geografiche (regione, codice provincia, ecc.)
 find "${folder}/../data/processing/${file}" -type f -name "*.csv" ! -name "omicidi_dcpc.csv" | while read -r csv_file; do
+  # Esegue il join tra i dati dei reati e le informazioni geografiche delle province
+  # - --ul: usa un left join per mantenere tutte le righe del file di input
+  # - unsparsify: riempie le celle vuote con valori di default
+  # - sort/reorder: riorganizza i dati in modo coerente
   mlr --csv join --ul -j provincia -f "${csv_file}" then unsparsify then sort -t anno,provincia,delitto,descrizione_reato,eta_alla_data_del_reato_vittima then reorder -f anno,provincia,delitto,descrizione_reato,eta_alla_data_del_reato_vittima "${folder}"/../resources/province_sdi_istat.csv >"${folder}"/tmp/tmp.csv
   mv "${folder}"/tmp/tmp.csv "${csv_file}"
 done
